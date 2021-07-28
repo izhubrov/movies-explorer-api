fc2dbd7555bd093b8889934f02b4ca37ff9dfe46
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-28 17:47:47 +0300

    feat: auth

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { isCelebrateError } = require('celebrate');
