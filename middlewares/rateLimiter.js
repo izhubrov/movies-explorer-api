ea0a04a512b581856c5f868b209ccb6cb7ee0e6d
Ivan Zhubrov <izhubrov@rek-21.ru>
2021-07-31 08:18:37 +0300

    fix: rate limiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowsMs: 60 * 1000, // 1 minute
-  max: 20,
+  max: 100,
   message: { message: 'Превышено допустимое число запросов к серверу' },
 });
 
